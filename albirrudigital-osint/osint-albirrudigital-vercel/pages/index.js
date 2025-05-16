2644ac8c21ae06282b40082095aa2c11a8d06930
albirrudigital <170484693+albirrudigital@users.noreply.github.com>
2025-05-16 16:51:45 +0000

    Add files via upload

---
+++ albirrudigital-osint/osint-albirrudigital-vercel/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-50 text-center">
+      <h1 className="text-4xl font-bold text-blue-800">OSINT Albirru Digital Teknology</h1>
+    </div>
+  )
+}
